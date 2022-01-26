98c9e3d9496a4bf82981c6aa912df5d6263f94ed
nicholasjamesbaker <91539245+nicholasjamesbaker@users.noreply.github.com>
2022-01-26 14:58:49 -0330

    Second version of lightbulb code - MUCH BETTER!

--- Lightbulb/Lightbulb.py
+++ Lightbulb/Lightbulb.py
@@ -1,10 +1,11 @@
 class Lightbulb:
-    def __init__(self, wattage = 0, is_led = bool, brand_name = "", is_on = bool):
+    def __init__(self, wattage: int, is_led: bool, brand_name: str, is_on: bool = False, brightness: int = 0):
         print("init method is invoked")
         self.wattage = wattage
         self.is_led = is_led
         self.brand_name = brand_name
-        self.is_on = False
+        self.is_on = is_on
+        self.brightness = brightness
 
     def turn_on(self):
         self.is_on = True
@@ -13,7 +14,19 @@ class Lightbulb:
         self.is_on = False
 
     def to_string(self):
-        print(self.wattage, self.is_led, self.brand_name)
+        print(f"Lightbulb wattage is {self.wattage}, is it a LED? {self.is_led}"
+              f", The brand is {self.brand_name}, the brightness level is {self.brightness}")
 
     def set_brightness(self, level):
-        level = int(input("Set brightness level (0-100)"))
+        assert 0 <= level <= 10, "Level outside bounds"
+        self.brightness = level
+
+
+Lightbulb = Lightbulb(60, True, "EcoSmart", False, 0)
+
+Lightbulb.turn_on()
+
+Lightbulb.set_brightness(9)
+
+Lightbulb.to_string()
+
