4838ad17eaf5ad18959e42085c863d6219fb226b
nicholasjamesbaker <91539245+nicholasjamesbaker@users.noreply.github.com>
2022-02-14 09:59:50 -0330

    Fixed with 3 level hierarchy

--- Inheritance/publication.py
+++ Inheritance/publication.py
@@ -4,6 +4,13 @@ class Publication:
         self.price = price
 
 
+class Periodical(Publication):
+    def __init__(self, period: str, publisher: str, title: str, price: float):
+        super().__init__(title, price)
+        self.period = period
+        self.publisher = publisher
+
+
 class Book(Publication):
     def __init__(self, author: str, pages: int, title: str, price: float):
         super().__init__(title, price)
@@ -11,23 +18,19 @@ class Book(Publication):
         self.pages = pages
 
 
-class Newspaper(Publication):
-    def __init__(self, publisher: str, pages: int, title: str, price: float):
-        super().__init__(title, price)
-        self.publisher = publisher
-        self.pages = pages
+class Newspaper(Periodical):
+    def __init__(self, publisher: str, title: str, price: float, period: str):
+        super().__init__(period, publisher, title, price)
 
 
-class Magazine(Publication):
-    def __init__(self, publisher: str, pages: int, title: str, price: float):
-        super().__init__(title, price)
-        self.publisher = publisher
-        self.pages = pages
+class Magazine(Periodical):
+    def __init__(self, publisher: str, title: str, price: float, period: str):
+        super().__init__(period, publisher, title, price)
 
 
 b1 = Book("Aldous Huxley", 311, "Brave New World", 29.0)
-n1 = Newspaper("New York Times Company", 10, "NY Times", 6.0)
-m1 = Magazine("Springer Nature", 40, "Scientific American", 5.99)
+n1 = Newspaper("New York Times Company", "NY Times", 6, "Daily")
+m1 = Magazine("Springer Nature", "Scientific American", 5.99, "Monthly")
 
 print(b1.author)
 print(n1.publisher)
